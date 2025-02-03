dd9cfd2c8044fa959eee0f0b190fd0357056fdc4
Eugenio <eugeniopch@gmail.com>
2025-02-03 19:37:17 -0300

    Agregué Web Worker y cambié modelo de IA

---
+++ worker.js
@@ -0,0 +1,8 @@
+// worker.ts
+import { WebWorkerMLCEngineHandler } from "https://cdn.jsdelivr.net/npm/@mlc-ai/web-llm@0.2.46/+esm"
+
+const handler = new WebWorkerMLCEngineHandler()
+
+self.onmessage = (msg) => {
+  handler.onmessage(msg)
+}
